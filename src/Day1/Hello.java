24bec9e0890264645fc6b502f50f70f58f87dc3a
Ed Harcourt <edharcourt@gmail.com>
2023-01-19 09:34:05 -0500

    My first git commit for Hellow World.

---
+++ src/Day1/Hello.java
@@ -0,0 +1,13 @@
+package Day1;
+
+// All code in Java is organized into classes
+public class Hello {
+
+    // function that starts the program
+    // psvm <tab> fills in the function
+    // declaration
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
