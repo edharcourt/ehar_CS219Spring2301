1c32229c450e2e2c6dc9fb867bfc192b5d063806
Ed Harcourt <edharcourt@gmail.com>
2023-01-19 09:58:33 -0500

    Messes around with syntax.

--- src/Day1/Hello.java
+++ src/Day1/Hello.java
@@ -6,8 +6,20 @@ public class Hello {
     // function that starts the program
     // psvm <tab> fills in the function
     // declaration
+    // Reading Ch 1 and Ch 2
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int x = 9;
+
+        // semicolons indicate the end of a
+        /// Java statement
+        if (x > 0) {
+            x = x + 1;
+            System.out.println(x);
+        }
+
+
     }
 
 }
