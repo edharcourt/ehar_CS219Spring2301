6515ea573306e308677d263f1ba1c7076438eafe
Ed Harcourt <edharcourt@gmail.com>
2023-01-24 11:36:23 -0500

    Work from Day 2 on Java's variable, types, and expressions.

--- src/Day1/Hello.java
+++ src/Day1/Hello.java
@@ -8,18 +8,8 @@ public class Hello {
     // declaration
     // Reading Ch 1 and Ch 2
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        int x = 9;
-
-        // semicolons indicate the end of a
-        /// Java statement
-        if (x > 0) {
-            x = x + 1;
-            System.out.println(x);
-        }
-
-
+        System.out.println("ello Sorld!");
+        //System.out.println(7/0);
     }
 
 }
