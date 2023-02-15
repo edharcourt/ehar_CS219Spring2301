a4a404986405a36db264fcf14b6aa22ca7d9e541
edharcourt@gmail.com <edharcourt@gmail.com>
2023-02-15 14:15:42 -0500

    updated HW 5 to add isDoubloon

--- src/HW5/Exercise65_66.java
+++ src/HW5/Exercise65_66.java
@@ -1,12 +1,27 @@
 package HW5;
 
-public class Exercise66 {
+public class Exercise65_66 {
+
+    public static boolean isDoubloon(String s) {
+
+        return true;
+    }
 
     public static boolean canSpell(String word, String tiles) {
         return false; // squelch error message about no return statement
     }
 
     public static void main(String[] args) {
+
+        System.out.println(isDoubloon("Abba"));
+        System.out.println(isDoubloon("appeases"));
+        System.out.println(isDoubloon("applesauce"));
+        System.out.println(isDoubloon("esophagographers"));
+
+        // Negative tests
+        System.out.println(!isDoubloon("esophagographer"));
+        System.out.println(!isDoubloon("bananab"));
+
         System.out.println(canSpell("boot", "axobasrto"));
         System.out.println(!canSpell("hippo", "haxobapsrito"));
 
