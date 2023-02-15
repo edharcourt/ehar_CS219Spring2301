0d03ffa29cfbcb5a37dda51794943daf2a95cae2
edharcourt@gmail.com <edharcourt@gmail.com>
2023-02-15 13:29:42 -0500

    HW 5

---
+++ src/HW5/Exercise66.java
@@ -0,0 +1,18 @@
+package HW5;
+
+public class Exercise66 {
+
+    public static boolean canSpell(String word, String tiles) {
+        return false; // squelch error message about no return statement
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canSpell("boot", "axobasrto"));
+        System.out.println(!canSpell("hippo", "haxobapsrito"));
+
+        // Student: add more test cases including negative tests.
+        // All tests should print true if the test passes.
+
+    } // main
+
+} // class Exercise66
