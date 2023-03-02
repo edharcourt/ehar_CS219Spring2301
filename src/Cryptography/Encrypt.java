ee34e1c233d0c5d38da8c19ce223a0710c5ce294
Ed Harcourt <edharcourt@gmail.com>
2023-03-02 09:57:22 -0500

    Finished crypto example

--- src/Cryptography/Encrypt.java
+++ src/Cryptography/Encrypt.java
@@ -1,33 +1,54 @@
 package Cryptography;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class Encrypt {
 
     // Symmetric cryptography, sender and receiver
     // use a shared key.
 
-    public static String E(String clear, int key) {
+    public static String E(String clear, int key, boolean enc) {
         String cipher = "";
+
+        SecureRandom rng = null;
+        try {
+            rng = SecureRandom.getInstance("SHA1PRNG");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Error: could not create random number generator");
+            System.exit(1);  // stop the Java program
+        }
+
+        rng.setSeed(key);
+        final int alpha_length = '~' - ' ' + 1;
+        final char base_char = ' ';
 
         for (int i = 0; i < clear.length(); i++) {
             char clear_char = clear.charAt(i);
-            int clear_char_pos = clear_char - 'a';
+            int clear_char_pos = clear_char - base_char;
+            int r = rng.nextInt();
+            if (!enc) r = -r;
             int cipher_char_pos =
-                Math.floorMod(clear_char_pos + key, 26);
-            char cipher_char = (char) (cipher_char_pos + 'a');
+                Math.floorMod(clear_char_pos + r, alpha_length);
+            char cipher_char = (char) (cipher_char_pos + base_char);
             cipher = cipher + cipher_char;
         }
         return cipher;
     }
 
     public static void main(String[] args) {
-        System.out.println(
-            E("hello", 0).equals("hello"));
-        System.out.println(E("hello", 1).equals("ifmmp"));
-        System.out.println(E("hello", 26).equals("hello"));
+//        System.out.println(
+//            E("hello", 0).equals("hello"));
+//        System.out.println(E("hello", 1).equals("ifmmp"));
+//        System.out.println(E("hello", 26).equals("hello"));
+//
+//        String t = E("attackatdawn", 13);
+//        System.out.println(t);
+//        System.out.println(E(t,-13));
 
-        String t = E("attackatdawn", 13);
-        System.out.println(t);
-        System.out.println(E(t,-13));
+        System.out.println(E("Attack! at dawn :-)", 99, true));
+        System.out.println(E("wHm;%7;Y@:2AB3}!`D7", 99, false));
 
     }
 
--- src/Cryptography/ModularPlay.java
+++ src/Cryptography/ModularPlay.java
@@ -1,14 +1,23 @@
 package Cryptography;
 
+import java.util.Random;
+
 public class ModularPlay {
 
     public static void main(String[] args) {
-        System.out.println(2 % 10 == 2);
-        System.out.println(-2 % 10);
-        System.out.println(-1/2);
+//        System.out.println(2 % 10 == 2);
+//        System.out.println(-2 % 10);
+//        System.out.println(-1/2);
+//
+//        System.out.println(Math.floorMod(-15,12));
+//        System.out.println(Math.floorDiv(-15,12));
 
-        System.out.println(Math.floorMod(-15,12));
-        System.out.println(Math.floorDiv(-15,12));
+        Random rng = new Random();
+        rng.setSeed(42);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(rng.nextInt(26));
+        }
+
     }
 
 }
