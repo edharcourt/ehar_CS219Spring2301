931ce335baeb6232ba089a06176b776b836f2b78
Ed Harcourt <edharcourt@gmail.com>
2023-03-14 11:30:58 -0400

    Exercises.Banish
    MyArrays.shift
    MyArrays.indexOf

--- src/Utility/MyArrays.java
+++ src/Utility/MyArrays.java
@@ -4,6 +4,27 @@ import java.util.Arrays;
 
 public class MyArrays {
 
+    public static int indexOf(int [] values,
+                              int key) {
+        for (int i = 0; i < values.length; i++)
+            if (values[i] == key)
+                return i;
+
+        return -1;
+    }
+
+    // shift all of the values in the array
+    // start at index i
+    // left, by one place, filling in the last
+    // value as zero.
+    public static int [] shift(int [] values, int i) {
+        for (int j = i; j < values.length - 1; j++)
+            values[j] = values[j+1];
+
+        values[values.length - 1] = 0;
+        return values;
+    }
+
     public static double median(double [] array) {
 
         Arrays.sort(array);
@@ -56,6 +77,21 @@ public class MyArrays {
             System.out.print(array[i] + ", ");
         System.out.print(array[array.length - 1]);
         System.out.println(']');
+    }
+
+    public static void main(String[] args) {
+        // run some tests
+        int [] array = {4, 7, 6, 9, 1, 7};
+        System.out.println(indexOf(array, 9) == 3);
+        System.out.println(indexOf(array, 43) == -1);
+        System.out.println(indexOf(array, 7) == 1);
+
+        shift(array, 2);
+        System.out.println(Arrays.toString(array).
+          equals(Arrays.toString(new int [] {4,7,9,1,7,0})));
+
+        System.out.println(
+            Arrays.compare(array,new int [] {4,7,9,1,7,0}) == 0);
     }
 
 }  // class MyArrays
